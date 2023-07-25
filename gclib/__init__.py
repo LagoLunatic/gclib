165ac67f54ef96f643fd5a3b94da1df3e0e88a06
LagoLunatic <LagoLunatic@users.noreply.github.com>
2023-07-25 02:15:56 -0400

    Fix make module init all static
    Apparently making this dynamic breaks PyInstaller builds.

--- gclib/__init__.py
+++ gclib/__init__.py
@@ -2,13 +2,28 @@
 import os
 import glob
 
-__all__ = []
+__all__ = [
+  "bfn",
+  "bmg",
+  "bti",
+  "dol",
+  "fs_helpers",
+  "gcm",
+  "gx_enums",
+  "j3d",
+  "jpc",
+  "rarc",
+  "rel",
+  "texture_utils",
+  "yaz0",
+]
+
 for module_path in glob.glob(glob.escape(os.path.dirname(__file__)) + "/*.py"):
   module_name, file_ext = os.path.splitext(os.path.basename(module_path))
   assert file_ext == ".py"
   if module_name == "__init__":
     continue
-  __all__.append(module_name)
+  assert module_name in __all__, f"{module_name} missing from __init__"
 
 # We need to import all the file types so that they register themselves with the RARC class.
 from . import *
