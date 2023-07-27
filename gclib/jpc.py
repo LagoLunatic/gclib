e5f0318f0f2bf31d69bae42255f5296a8797036b
LagoLunatic <LagoLunatic@users.noreply.github.com>
2023-07-27 03:09:22 -0400

    Guess num texture IDs in TDB1 chunk

--- gclib/jpc.py
+++ gclib/jpc.py
@@ -518,14 +518,21 @@ class TDB1(JPAChunk):
     self.texture_filenames = [] # Leave this list empty for now, it will be populated after the texture list is read.
   
   def read_texture_ids(self, num_texture_ids):
-    if self.version == JPACVersion.JPAC2_10:
-      # TODO: not 100% sure of this, but it seems correct from testing
-      num_texture_ids = 2
+    num_texture_ids_is_guessed = False
+    if num_texture_ids is None:
+      # Guess how many textures there are based on the size of the TDB1 chunk minus the header.
+      num_texture_ids = ((self.size - TDB1_ID_LIST_OFFSET[self.version]) // 2)
+      num_texture_ids_is_guessed = True
     
     self.texture_ids = []
     for texture_id_index in range(num_texture_ids):
       texture_id = fs.read_u16(self.data, TDB1_ID_LIST_OFFSET[self.version] + texture_id_index*2)
       self.texture_ids.append(texture_id)
+    
+    if num_texture_ids_is_guessed and self.texture_ids[-1] == 0 and len(self.texture_ids) % 2 == 0:
+      # Sometimes there are 2 bytes of zero-padding at the end of this chunk that makes it look
+      # like it has one more texture ID than it really does. Remove it.
+      self.texture_ids = self.texture_ids[:-1]
   
   def save_chunk_specific_data(self):
     # Save the texture IDs (which were updated by the JPC's save_changes function).
