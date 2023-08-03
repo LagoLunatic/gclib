7ce4ba95bd28bbf14ad59f2aace104cee0a06b1c
LagoLunatic <LagoLunatic@users.noreply.github.com>
2023-08-03 01:49:58 -0400

    Add barebones JNT1 and SHP1 chunk implementations

--- gclib/j3d.py
+++ gclib/j3d.py
@@ -12,6 +12,8 @@ from gclib.gx_enums import GXAttr, GXComponentCount, GXCompType, GXCompTypeNumbe
 IMPLEMENTED_CHUNK_TYPES = [
   #"INF1",
   "VTX1",
+  "JNT1",
+  "SHP1",
   "TEX1",
   "MAT3",
   "MDL3",
@@ -590,6 +592,97 @@ class VTX1(J3DChunk):
           raise NotImplementedError
       data_offset += comp_size
     return data_offset
+
+class JNT1(J3DChunk):
+  def read_chunk_specific_data(self):
+    self.joint_count = fs.read_u16(self.data, 0x08)
+    self.joint_data_offset = fs.read_u32(self.data, 0x0C)
+    self.string_table_offset = fs.read_u32(self.data, 0x14)
+    
+    offset = self.joint_data_offset
+    self.joints = []
+    for joint_index in range(self.joint_count):
+      joint = Joint(self.data)
+      joint.read(offset)
+      self.joints.append(joint)
+      offset += Joint.DATA_SIZE
+    
+    self.joint_names = self.read_string_table(self.string_table_offset)
+    self.joints_by_name = OrderedDict()
+    for joint_index, joint in enumerate(self.joints):
+      joint_name = self.joint_names[joint_index]
+      self.joints_by_name[joint_name] = joint
+  
+  def save_chunk_specific_data(self):
+    pass
+
+class Joint:
+  DATA_SIZE = 0x40
+  
+  def __init__(self, data):
+    self.data = data
+  
+  def read(self, offset):
+    self.x_rot = fs.read_u16(self.data, offset+0x10)
+    self.y_rot = fs.read_u16(self.data, offset+0x12)
+    self.z_rot = fs.read_u16(self.data, offset+0x14)
+    
+    self.x = fs.read_float(self.data, offset+0x18)
+    self.y = fs.read_float(self.data, offset+0x1C)
+    self.z = fs.read_float(self.data, offset+0x20)
+    
+    self.bbox_min_x = fs.read_float(self.data, offset+0x28)
+    self.bbox_min_y = fs.read_float(self.data, offset+0x28)
+    self.bbox_min_z = fs.read_float(self.data, offset+0x28)
+    self.bbox_max_x = fs.read_float(self.data, offset+0x28)
+    self.bbox_max_y = fs.read_float(self.data, offset+0x28)
+    self.bbox_max_z = fs.read_float(self.data, offset+0x28)
+  
+  def save(self, offset):
+    pass
+
+class SHP1(J3DChunk):
+  def read_chunk_specific_data(self):
+    self.shape_count = fs.read_u16(self.data, 0x08)
+    self.shape_data_offset = fs.read_u32(self.data, 0x0C)
+    self.attribute_table_offset = fs.read_u32(self.data, 0x18)
+    self.matrix_table_offset = fs.read_u32(self.data, 0x1C)
+    self.primitive_data_offset = fs.read_u32(self.data, 0x20)
+    self.mtx_group_table_offset = fs.read_u32(self.data, 0x28)
+    
+    self.shapes = []
+    shape_offset = self.shape_data_offset
+    for shape_index in range(self.shape_count):
+      shape = Shape(self.data)
+      shape.read(shape_offset)
+      self.shapes.append(shape)
+      shape_offset += Shape.DATA_SIZE
+  
+  def save_chunk_specific_data(self):
+    pass
+
+class Shape:
+  DATA_SIZE = 0x28
+  
+  def __init__(self, data):
+    self.data = data
+  
+  def read(self, offset):
+    self.mtx_group_count = fs.read_u16(self.data, offset+0x02)
+    self.first_attribute_offset = fs.read_u16(self.data, offset+0x04)
+    self.first_mtx_group_index = fs.read_u16(self.data, offset+0x08)
+    
+    self.bounding_sphere_radius = fs.read_float(self.data, offset+0x0C)
+    
+    self.bbox_min_x = fs.read_float(self.data, offset+0x10)
+    self.bbox_min_y = fs.read_float(self.data, offset+0x14)
+    self.bbox_min_z = fs.read_float(self.data, offset+0x18)
+    self.bbox_max_x = fs.read_float(self.data, offset+0x1C)
+    self.bbox_max_y = fs.read_float(self.data, offset+0x20)
+    self.bbox_max_z = fs.read_float(self.data, offset+0x24)
+  
+  def save(self, offset):
+    pass
 
 class TEX1(J3DChunk):
   def read_chunk_specific_data(self):
